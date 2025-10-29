2dcec5443a38fbc308c0e77fb9aa3163fba78071
Mildred Mancilla <mildred.lopez5450@alumnos.udg.mx>
2025-10-29 14:55:40 -0600

    ML - traductor 8

---
+++ traductor8/src/act2/AnalizadorSintacticoJava.java
@@ -0,0 +1,33 @@
+package act2;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.io.InputStreamReader;
+
+public class AnalizadorSintacticoJava {
+    public static void main(String[] args) {
+        try {
+            Reader lector;
+            if (args.length > 0) {
+                lector = new BufferedReader(new FileReader(args[0]));
+            } else {
+                lector = new BufferedReader(new FileReader("src/act2/TestSintacticoCPP.txt"));
+            }
+
+            LexicoCPP scanner = new LexicoCPP(lector);
+            Sintactico parser = new Sintactico(scanner);
+
+            parser.parse();
+
+            System.out.println("Errores semánticos: " + parser.erroresSem);
+            System.out.println("Errores sintácticos: " + parser.erroresSint);
+            if (parser.erroresSem == 0 && parser.erroresSint == 0) {
+                System.out.println("Analisis realizado correctamente");
+            }
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+}
